c21db336473d1a8f8ae4746332900fd6a2263e60
Ayan Sayyad <98633937+Ayan02-coder@users.noreply.github.com>
2024-09-05 11:39:13 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+str = input("Enter a string: ")
+if str == str[::-1]:
+    print("palindrome")
+else:
+    print("not")
+
+#A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward.
